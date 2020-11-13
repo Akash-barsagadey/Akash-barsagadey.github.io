ce0517db37b0eae936fbe79a12179625ae13e349
Akash Barsagadey <akash_udaram.barsagadey@syngenta.com>
2020-11-13 21:44:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <title></title>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Akash Barsagadey | Principal Front End Software Engineer in Pune,India
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
